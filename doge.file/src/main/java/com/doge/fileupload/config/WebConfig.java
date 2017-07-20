37c11bd36be4b8861765e3fdf3813da02715bc4e
meddkim <meddkim@gmail.com>
2017-07-20 18:38:21 +0800

    add file

--- doge.file/src/main/java/com/doge/fileupload/config/WebConfig.java
+++ doge.file/src/main/java/com/doge/fileupload/config/WebConfig.java
@@ -1,11 +1,17 @@
 package com.doge.fileupload.config;
 
+
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 import javax.servlet.MultipartConfigElement;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author: Administrator
@@ -15,23 +21,33 @@ import javax.servlet.MultipartConfigElement;
 @Configuration
 public class WebConfig {
 
-    @Bean(name = "multipartResolver")
-    public CommonsMultipartResolver createMultipartResolver() {
-        CommonsMultipartResolver resolver=new CommonsMultipartResolver();
-        resolver.setDefaultEncoding("utf-8");
-        resolver.setMaxUploadSize(1000*1024*1024*1000L);
-        return resolver;
-    }
+//    @Bean(name = "multipartResolver")
+//    public CommonsMultipartResolver createMultipartResolver() throws IOException {
+//        CommonsMultipartResolver resolver=new CommonsMultipartResolver();
+//        resolver.setDefaultEncoding("utf-8");
+//        return resolver;
+//    }
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
         // 设置文件大小限制 ,超出设置页面会抛出异常信息，
         // 这样在文件上传的地方就需要进行异常信息的处理了;
-        factory.setMaxFileSize("256KB"); // KB,MB
+        factory.setMaxFileSize("1024MB"); // KB,MB
         /// 设置总上传数据总大小
-        factory.setMaxRequestSize("512KB");
-        // Sets the directory location where files will be stored.
-        // factory.setLocation("路径地址");
+        factory.setMaxRequestSize("10240MB");
+        // 在该项目中，一下目录为临时文件存储的路径，传输完成后，临时文件删除
+        String tempRootPath = System.getProperty("user.dir") + File.separator + "temp";
+        File file = new File(tempRootPath);
+        if(!file.exists()){
+            file.mkdir();
+        }
+        factory.setLocation(tempRootPath);
+//        factory.setFileSizeThreshold("10MB"); //设置缓存区有多少文件就写入到磁盘中
         return factory.createMultipartConfig();
+    }
+
+    @Bean(name = "multipartResolver")
+    public StandardServletMultipartResolver resolver() {
+        return new StandardServletMultipartResolver();
     }
 }
--- doge.file/src/main/java/com/doge/fileupload/controller/FileUploadController.java
+++ doge.file/src/main/java/com/doge/fileupload/controller/FileUploadController.java
@@ -2,25 +2,15 @@ package com.doge.fileupload.controller;
 
 import com.doge.fileupload.utils.UploadFileUtils;
 import com.doge.fileupload.utils.UploadPathUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.ServletContextAware;
+
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
-import java.util.List;
 
 /**
  * @author: Administrator
@@ -28,29 +18,15 @@ import java.util.List;
  * @Description:
  */
 @RestController
-//public class FileUploadController implements ServletContextAware{
 public class FileUploadController {
 
-//    private ServletContext servletContext;
 
-//    @Autowired
-//    private HttpServletRequest request;
-//    @Autowired
-//    private HttpServletResponse response;
-
-//    @Override
-//    public void setServletContext(ServletContext servletContext) {
-//        this.servletContext = servletContext;
-//    }
-
-    @RequestMapping(value = "/file/upload",method = RequestMethod.POST)
+    @RequestMapping(value = "/file/upload/multiple",method = RequestMethod.POST)
     public void upload(HttpServletRequest request) throws IOException{
         request.setCharacterEncoding("utf8");
 
-        //获取项目运行所在路径
-        String rootPath = System.getProperty("user.dir");
         //文件存储路径
-        String savePath = rootPath + File.separator + "upload";
+        String savePath =  System.getProperty("user.dir") + File.separator + "upload";
 
         System.out.println();
         //创建通用多部分解析器
@@ -83,17 +59,5 @@ public class FileUploadController {
         }else{
 
         }
-    }
-
-
-    @RequestMapping(value = "/file/upload",method = RequestMethod.GET)
-    public void test() throws IOException{
-//        request.setCharacterEncoding("utf8");
-//
-//        //获取项目运行所在路径
-//        String rootPath = System.getProperty("user.dir");
-//        //文件存储路径
-//        String savePath = rootPath + File.separator + "upload";
-//        System.out.println(savePath);
     }
 }
