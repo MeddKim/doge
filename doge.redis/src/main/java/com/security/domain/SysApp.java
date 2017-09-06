96baec541e2a9784b228fdeaeb9182f7d358bffa
wang.zhongyi <meddkim@163.com>
2017-09-06 11:12:05 +0800

    在整合redis时，实体类要实现Serializable接口完成序列化，否则报错

--- doge.redis/src/main/java/com/security/domain/SysApp.java
+++ doge.redis/src/main/java/com/security/domain/SysApp.java
@@ -2,12 +2,16 @@ package com.security.domain;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 来源于数据源 shiro3
  */
 
 @Data
-public class SysApp {
+public class SysApp implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private Integer id;
     private String name;
--- doge.redis/src/main/java/com/security/domain/UserRoles.java
+++ doge.redis/src/main/java/com/security/domain/UserRoles.java
@@ -2,11 +2,16 @@ package com.security.domain;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 来源于数据源shiro1
  */
 @Data
-public class UserRoles {
+public class UserRoles implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
     private String username;
     private String roleName;
