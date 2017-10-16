ff32f4e696f360d4d837c3ad99e3245b153805f4
wang.zhongyi <meddkim@163.com>
2017-10-16 15:43:15 +0800

    add redis demo

---
+++ doge.redis/src/main/java/com/security/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.security.utils;
+
+public class Constants {
+    public static final int AUTH_TIMEOUT = 5184000;  // 两个月（以60天计）秒数
+    public static final int SECONDS_OF_ONE_MONTH = 2592000;   //一个月（以30天计）秒数
+    public static final int SECONDS_OF_TEN_DAY = 864000;  //十天秒数
+    public static final int SECONDS_OF_ONE_DAY = 86400;   //一天秒数
+    public static final int SECONDS_OF_HALF_AN_HOUR = 1800;   //半小时秒数
+
+    public static final String SPACE = " ";
+    public static final String VERTICAL_LINE = "|";
+    public static final String DASH = "-";
+    public static final String UNDERSCORE = "_";
+    public static final String COMMA = ",";
+    public static final String EMPTY = "";
+}
